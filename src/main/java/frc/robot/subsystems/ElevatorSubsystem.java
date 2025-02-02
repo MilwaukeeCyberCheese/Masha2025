6b9ba60a1f1be278a9029f3bb4df1b580bada719
Sam948-byte <techguy763@gmail.com>
2025-02-02 00:46:30 -0600

    initialize

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -2,4 +2,22 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ElevatorSubsystem extends SubsystemBase {}
+public class ElevatorSubsystem extends SubsystemBase {
+
+    public ElevatorSubsystem() {
+        // Initialize motors, sensors, etc. here
+    }
+    
+    // Methods to set motor speeds, etc. go here
+    
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+
+        log();
+    }
+
+    public void log() {
+        // Log sensor data, etc. here
+    }
+}
