c7a8e54ab2e46aae8f4aa9e56f6fa568b56967db
HENRYMARTIN5 <henrysjmartinthesecond@gmail.com>
2025-02-03 20:02:33 -0600

    feat!: Actual sorta functional scoring

--- src/main/java/frc/robot/subsystems/CoralHandlerSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralHandlerSubsystem.java
@@ -4,15 +4,24 @@ import com.revrobotics.Rev2mDistanceSensor.Unit;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Handler.Coral;
 import frc.robot.Constants.Sensors;
 import frc.robot.Robot;
+
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Radians;
+
 import org.dyn4j.geometry.Rectangle;
 import org.dyn4j.geometry.Vector2;
 import org.ironmaple.simulation.IntakeSimulation;
+import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.drivesims.AbstractDriveTrainSimulation;
+import org.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeCoralOnFly;
 
 public class CoralHandlerSubsystem extends SubsystemBase {
   // two neo 550s, 9:1 gearing on both, going in opposite directions.
@@ -22,6 +31,7 @@ public class CoralHandlerSubsystem extends SubsystemBase {
   // vice versa for the other
 
   public final IntakeSimulation m_intakeSim;
+  private final AbstractDriveTrainSimulation m_drive;
 
   public enum CoralHandlerState {
     kInactive,
@@ -33,7 +43,7 @@ public class CoralHandlerSubsystem extends SubsystemBase {
   private boolean m_hasCoral = false;
 
   public CoralHandlerSubsystem(AbstractDriveTrainSimulation driveSim) {
-
+    m_drive = driveSim;
     Coral.m_left.configure(
         Coral.m_leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     Coral.m_right.configure(
@@ -96,7 +106,19 @@ public class CoralHandlerSubsystem extends SubsystemBase {
       }
       if (m_state == CoralHandlerState.kRelease && m_intakeSim.obtainGamePieceFromIntake()) {
         // TODO: make this dependent on elevator position
-
+        SimulatedArena
+            .getInstance()
+            .addGamePieceProjectile(
+              new ReefscapeCoralOnFly(
+                m_drive.getSimulatedDriveTrainPose().getTranslation(),
+                new Translation2d(0.35, 0), // mechanism position, this is where elevator comes into play i think
+                m_drive.getDriveTrainSimulatedChassisSpeedsFieldRelative(),
+                m_drive.getSimulatedDriveTrainPose().getRotation(),
+                Meters.of(1.28), // eject height
+                MetersPerSecond.of(2), // eject speed
+                Radians.of(0.610865238)
+              )
+            );
       }
       m_hasCoral = m_intakeSim.getGamePiecesAmount() != 0;
     }
