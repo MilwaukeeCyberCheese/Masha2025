5439511ecef866a8d7c43aae34d1953523180e12
Sam948-byte <techguy763@gmail.com>
2025-03-15 16:53:31 -0500

    chore: remove algae handler

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,8 +22,6 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.Servo;
-import frc.robot.subsystems.AlgaeHandlerSubsystem.AlgaeHandlerIntakeState;
-import frc.robot.subsystems.AlgaeHandlerSubsystem.AlgaeHandlerPositionState;
 import frc.robot.subsystems.ClimberSubsystem.ClimberState;
 import frc.robot.subsystems.CoralHandlerSubsystem.CoralHandlerState;
 import frc.robot.subsystems.ElevatorSubsystem.ElevatorState;
@@ -220,83 +218,6 @@ public final class Constants {
             .maxAcceleration(kPIDConstants.kMaxAcceleration);
 
         kRightConfig.apply(kLeftConfig).inverted(kRightInverted);
-      }
-    }
-
-    public static final class Algae {
-      public static final int kPositionMotorCanId = 13;
-      public static final int kIntakeMotorCanId = 14;
-
-      public static final SparkMax kPositionSparkMax =
-          new SparkMax(kPositionMotorCanId, SparkMax.MotorType.kBrushless);
-      public static final SparkMax kIntakeSparkMax =
-          new SparkMax(kIntakeMotorCanId, SparkMax.MotorType.kBrushless);
-
-      public static final SparkMaxConfig kPositionConfig = new SparkMaxConfig();
-      public static final SparkMaxConfig kIntakeConfig = new SparkMaxConfig();
-
-      // TODO: find out if it's inverted
-      public static final boolean kPositionInverted = false;
-      public static final boolean kIntakeInverted = false;
-
-      public static final SparkClosedLoopController kPositionController =
-          kPositionSparkMax.getClosedLoopController();
-      public static final SparkClosedLoopController kIntakeController =
-          kIntakeSparkMax.getClosedLoopController();
-
-      public static final PIDConstants kPositionPIDConstants = new PIDConstants(0.1, 0.0, 0.0);
-      public static final PIDConstants kIntakePIDConstants = new PIDConstants(0.1, 0.0, 0.0);
-
-      // TODO: confirm that this is right
-      public static final double kPositionConversionFactor = Math.PI * 2;
-      public static final double kIntakeConversionFactor = 1.0;
-
-      // TODO: find these
-      public static final HashMap<AlgaeHandlerPositionState, Double> kPositions =
-          new HashMap<AlgaeHandlerPositionState, Double>() {
-            {
-              put(AlgaeHandlerPositionState.STOWED, 0.0);
-              put(AlgaeHandlerPositionState.GRAB_FROM_REEF, 0.0);
-              put(AlgaeHandlerPositionState.GRAB_FROM_GROUND, 0.0);
-            }
-          };
-      public static final HashMap<AlgaeHandlerIntakeState, Double> kSpeeds =
-          new HashMap<AlgaeHandlerIntakeState, Double>() {
-            {
-              put(AlgaeHandlerIntakeState.INTAKE, 0.0);
-              put(AlgaeHandlerIntakeState.OUTTAKE, 0.0);
-              put(AlgaeHandlerIntakeState.STOPPED, 0.0);
-            }
-          };
-
-      // TODO: find this
-      public static final double[] kPositionLimits = {0.0, 0.0};
-
-      // TODO: find this
-      public static final double kPositionTolerance = 0.01;
-      public static final double kIntakeTolerance = 10;
-
-      static {
-        kPositionConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).inverted(kPositionInverted);
-        kPositionConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).inverted(kIntakeInverted);
-
-        kPositionConfig
-            .absoluteEncoder
-            .positionConversionFactor(kPositionConversionFactor)
-            .velocityConversionFactor(kPositionConversionFactor / 60.0);
-        kIntakeConfig.encoder.velocityConversionFactor(kIntakeConversionFactor / 60);
-
-        kPositionConfig
-            .closedLoop
-            .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-            .pid(kPositionPIDConstants.kP, kPositionPIDConstants.kI, kPositionPIDConstants.kD)
-            .outputRange(-1, 1);
-
-        kIntakeConfig
-            .closedLoop
-            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-            .pid(kIntakePIDConstants.kP, kIntakePIDConstants.kI, kIntakePIDConstants.kD)
-            .outputRange(-1, 1);
       }
     }
   }
