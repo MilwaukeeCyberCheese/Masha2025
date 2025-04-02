b77c54d35c79d683e01589ac656fd021d9407508
samfreund <techguy763@gmail.com>
2025-04-02 10:38:43 -0500

    linting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ import choreo.auto.AutoFactory;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -176,5 +175,5 @@ public class RobotContainer {
     m_elevator.L1();
     m_climber.inactive();
     m_coral.inactive();
-}
+  }
 }
